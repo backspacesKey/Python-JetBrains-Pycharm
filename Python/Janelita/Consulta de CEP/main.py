cc625647afbb921237ee80af21d2ef1fe39cb8a0
backspacesKey <76973237+backspacesKey@users.noreply.github.com>
2022-07-20 11:26:22 -0300

    Fazendo sutis mudanças

--- Python/Janelita/Consulta de CEP/main.py
+++ Python/Janelita/Consulta de CEP/main.py
@@ -1,6 +1,8 @@
 import requests
 
 from time import sleep
+
+
 def main():
     print('')
     print('\033[0;33;40m#####################')
@@ -10,13 +12,15 @@ def main():
 
     cep_input = input('Digite o Cep Com 8 Dígitos: \033[m')
     if len(cep_input) > 8:
-        print('\033[0;31;40m Erro 0001: O CEP digitado contém mais de 8 dígitos.\033[m')
+        print(
+            '\033[0;31;40m Erro 0001: O CEP digitado contém mais de 8 dígitos.\033[m')
         print('\033[1;34;40mTentando Novamente\033[m')
         sleep(3)
         main()
 
     if len(cep_input) < 8:
-        print('\033[0;31;40m Erro 0001: O CEP digitado contém menos de 8 dígitos.\033[m')
+        print(
+            '\033[0;31;40m Erro 0001: O CEP digitado contém menos de 8 dígitos.\033[m')
         print('\033[1;34;40mTentando Novamente\033[m')
         sleep(3)
         main()
@@ -37,7 +41,8 @@ def main():
         sleep(1)
         print('Bairro: {}'.format(adrees_data['bairro']))
         sleep(1)
-        print('Cidade e UF: {} & {}'.format(adrees_data['localidade'], adrees_data['uf']))
+        print('Cidade e UF: {} & {}'.format(
+            adrees_data['localidade'], adrees_data['uf']))
         sleep(1)
         print('DDD: {}\033[m'.format(adrees_data['ddd']))
         print('')
@@ -47,14 +52,15 @@ def main():
         print(f'{cep_input}: O CEP Dígitado não é um lugar.')
 
     print('----'*5)
-    opção = int(input('Você Deseja Realizar Outra Operação? \n1. Sim \n2. Sair\n>>> '))
+    opção = int(
+        input('Você Deseja Realizar Outra Operação? \n1. Sim \n2. Sair\n>>> '))
 
     if opção == 1:
         main()
     if opção == 2:
-        print('\033[0;34;40m SSaindo...\033[m')
+        print('\033[0;34;40m Saindo...\033[m')
         exit()
+
+
 if __name__ == '__main__':
-	main()
-
-
+    main()
--- Python/Janelita/Janela de Permissão de entrada/entrada.py
+++ Python/Janelita/Janela de Permissão de entrada/entrada.py
@@ -4,17 +4,17 @@ from time import *
 #from playsound import playsound
 
 
-#Layout
+# Layout
 sg.theme('Reddit')
 layout = [
     [sg.Text("Usuario:"), sg.Input(key='usuario', size=(46, 1))],
-    [sg.Text("Senha:"), sg.Input(key='senha',password_char='*', size=(47, 1))],
+    [sg.Text("Senha:"), sg.Input(key='senha', password_char='*', size=(47, 1))],
     [sg.Checkbox('Salvar o Login?')],
-    [sg.Button('Entrar'),sg.Button('Cancelar')]
+    [sg.Button('Entrar'), sg.Button('Cancelar')]
 ]
-#janela
+# janela
 janela = sg.Window("Permissão de Entrada", layout)
-#Ler os Eventos
+# Ler os Eventos
 while True:
     eventos, valores = janela.read()
     if eventos == 'Cancelar':
@@ -29,31 +29,37 @@ while True:
         break
     if eventos == 'Entrar':
         if valores['usuario'] == 'Pedro Luiz' and valores['senha'] == 'PASSWORD':
-            sg.popup("ENTRADA APROVADA, PEDRO ESTA PERMITIDO ENTRAR NA CASA MEDEIROS.")
+            sg.popup(
+                "Bem vindo, " + valores['usuario'])
             sleep(0.1)
             break
         elif valores['usuario'] == 'Rafael' and valores['senha'] == 'Rafael':
-            sg.popup('ENTRADA APROVADA, RAFAEL ESTA PERMITIDO ENTRAR NA CASA MEDEIROS.')
+            sg.popup(
+                'Bem vindo, ' + valores['usuario'])
             sleep(0.1)
             break
         elif valores['usuario'] == 'Sergio' and valores['senha'] == 'Sergio':
-            sg.popup('ENTRADA APROVADA, SERGIO ESTA PERMITIDO ENTRAR NA CASA MEDEIROS.')
+            sg.popup(
+                'Bem vindo, ' + valores['usuario'])
             sleep(0.1)
             break
         elif valores['usuario'] == 'Ana Luiza' and valores['senha'] == 'Ana':
-            sg.popup('ENTRADA APROVADA, ANA LUIZA ESTA PERMITIDO ENTRAR NA CASA MEDEIROS.')
+            sg.popup(
+                'Bem vindo, ' + valores['usuario'])
             sleep(0.1)
             break
         elif valores['usuario'] == 'Maria Da Luz' and valores['senha'] == 'Maria':
-            sg.popup('ENTRADA APROVADA, MARIA DA LUZ ESTA PERMITIDO ENTRAR NA CASA MEDEIROS.')
+            sg.popup(
+                'Bem vindo, ' + valores['usuario'])
             sleep(0.1)
             break
         elif valores['usuario'] == 'Zete' and valores['senha'] == 'Zete':
-            sg.popup('ENTRADA APROVADA, MARIZETE ESTÁ PERMITIDA ENTRAR NA CASA MEDEIROS.')
+            sg.popup(
+                'Bem vindo, ' + valores['usuario'])
             sleep(0.1)
             break
         else:
-            sg.popup('LOGIN INCORRETO. REESCREVA O SEU LOGIN CORRETAMENTE.')
+            sg.popup('Usuário ou senha incorretos!')
             sleep(0.58)
-            break
-#FIM DO CÓGIGO
+            continue
+# FIM DO CÓGIGO
